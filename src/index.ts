f1b06e22a0fa9cf3ed82b6b57841b059ca70e9ea
Sebastian Springer <info@sebastian-springer.com>
2017-09-12 21:51:50 +0200

    express and typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello World');
+import * as express from 'express';
+import { createServer } from 'spdy';
+import { readFileSync } from 'fs';
+
+const app: express.Application = express();
+const options = {
+    cert: readFileSync('./certs/certificate.pem'),
+    key: readFileSync('./certs/key.pem')
+};
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('Hello Client');
+});
+
+const server = createServer(options, app);
+server.listen(8080, () => {
+    console.log('Server is listening to http://localhost:8080');
+});
