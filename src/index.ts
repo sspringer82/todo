030af2b79be9796093c5a37460fd59428ffd534d
Sebastian Springer <info@sebastian-springer.com>
2017-09-12 21:53:18 +0200

    it's https

--- src/index.ts
+++ src/index.ts
@@ -16,5 +16,5 @@ app.get('/', (req: express.Request, res: express.Response) => {
 
 const server = createServer(options, app);
 server.listen(8080, () => {
-    console.log('Server is listening to http://localhost:8080');
+    console.log('Server is listening to https://localhost:8080');
 });
