1fa208c9c340e3923f6c1623c1b4bc51a1345538
Sebastian Springer <info@sebastian-springer.com>
2017-09-13 18:16:12 +0200

    use helmet for additional security

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { createServer } from 'spdy';
 import { readFileSync } from 'fs';
 import { attachAccessLogger } from './accessLogger';
 import { logger } from './logger';
+import * as helmet from 'helmet';
 
 const app: express.Application = express();
 const options = {
     cert: readFileSync('./certs/certificate.pem'),
     key: readFileSync('./certs/key.pem')
 };
+app.use(helmet());
 
 app.use(express.static('/public'));
 attachAccessLogger(app);
