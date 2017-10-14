dc22d70e7015c5343616c411de7f638d5cc664b0
Sebastian Springer <info@sebastian-springer.com>
2017-10-14 14:38:38 +0200

    even more backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,8 +16,8 @@ const app: express.Application = express();
 app.set('view engine', 'ejs');
 initializePassport(app);
 const options = {
-    cert: readFileSync('./certs/certificate.pem'),
-    key: readFileSync('./certs/key.pem')
+  cert: readFileSync('./certs/certificate.pem'),
+  key: readFileSync('./certs/key.pem'),
 };
 
 app.use(helmet());
@@ -26,29 +26,29 @@ app.use(express.static('./public'));
 attachAccessLogger(app);
 
 app.post(
-    '/login',
-    passport.authenticate('local', { failureRedirect: '/' }),
-    (req: express.Request, res: express.Response) => {
-        res.redirect('/secret');
-    }
+  '/login',
+  passport.authenticate('local', { failureRedirect: '/' }),
+  (req: express.Request, res: express.Response) => {
+    res.redirect('/secret');
+  },
 );
 
 app.get('/logout', (req: express.Request, res: express.Response) => {
-    req.logout();
-    res.redirect('/');
+  req.logout();
+  res.redirect('/');
 });
 
 app.get(
-    '/secret',
-    ensureLoggedIn({ redirectTo: '/' }),
-    (req: express.Request, res: express.Response) => {
-        res.send('Hello Client');
-    }
+  '/secret',
+  ensureLoggedIn({ redirectTo: '/' }),
+  (req: express.Request, res: express.Response) => {
+    res.send('Hello Client');
+  },
 );
 
-app.use('/todo', , ensureLoggedIn({ redirectTo: '/' }), todoRouter);
+app.use('/todo', ensureLoggedIn({ redirectTo: '/' }), todoRouter);
 
 const server = createServer(options, app);
 server.listen(8080, () => {
-    logger.info('Server started');
+  logger.info('Server started');
 });
--- backend/src/todo/controller.ts
+++ backend/src/todo/controller.ts
@@ -1,26 +1,33 @@
+import * as express from 'express';
+
 const controller = {
-    getAll(req: Request, res: Response) {
-        this.render('Hello express');
-    },
-    getById(req: Request, res: Response) {
-        this.render('Hello express');
-    },
-    create(req: Request, res: Response) {
-        this.render('Hello express');
-    },
-    update(req: Request, res: Response) {
-        this.render('Hello express');
-    },
-    delete(req: Request, res: Response) {
-        this.render('Hello express');
-    },
-    render(req: Request, res: Response, data: any, view: string) {
-        if (req.accepts('json')) {
-            res.json(data);
-        } else {
-            res.render(view);
-        }
+  getAll(req: express.Request, res: express.Response) {
+    this.render(req, res, 'Hello Express');
+  },
+  getById(req: express.Request, res: express.Response) {
+    this.render(req, res, 'Hello Express');
+  },
+  create(req: express.Request, res: express.Response) {
+    this.render(req, res, 'Hello Express');
+  },
+  update(req: express.Request, res: express.Response) {
+    this.render(req, res, 'Hello Express');
+  },
+  delete(req: express.Request, res: express.Response) {
+    this.render(req, res, 'Hello Express');
+  },
+  render(
+    req: express.Request,
+    res: express.Response,
+    data: any,
+    view: string = '',
+  ) {
+    if (req.accepts('json')) {
+      res.json(data);
+    } else {
+      res.render(view);
     }
+  },
 };
 
 export { controller };
