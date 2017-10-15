f3d9adc266d4f3e66892e7a417fb0e871632b0ac
Sebastian Springer <info@sebastian-springer.com>
2017-10-15 11:45:13 +0200

    fetch a certain todo

--- api/src/todo/todo.controller.ts
+++ api/src/todo/todo.controller.ts
@@ -12,7 +12,15 @@ const controller = {
     res.json(todos);
     return todos;
   },
-  getOneAction(req: express.Request, res: express.Response): void {},
+  async getOneAction(
+    req: express.Request,
+    res: express.Response,
+  ): Promise<Todo> {
+    const id = parseInt(req.params.id, 10);
+    const todo = await todoModel.getOne(id);
+    res.json(todo);
+    return todo;
+  },
   createAction(req: express.Request, res: express.Response): void {},
   updateAction(req: express.Request, res: express.Response): void {},
   deleteAction(req: express.Request, res: express.Response): void {},
