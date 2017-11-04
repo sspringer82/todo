17226057098a64e30656f258878842b17e73c38e
Sebastian Springer <info@sebastian-springer.com>
2017-11-04 19:56:35 +0100

    FE: user list component

---
+++ angular/src/app/user/components/list/list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Observable } from 'rxjs/Observable';
+import { User } from '../../model/user';
+import { DataSource } from '@angular/cdk/table';
+import { UserDataSource } from '../../model/user-data-source';
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css'],
+})
+export class ListComponent implements OnInit {
+  public lists: Observable<User[]>;
+  public dataSource: UserDataSource;
+  public displayedColumns = ['username', 'isActive', 'isAdmin'];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.lists = this.userService.items;
+    this.dataSource = new UserDataSource(this.lists);
+    this.userService.load().subscribe(null, e => {
+      console.log(e);
+    });
+  }
+
+  public delete(listItem: User) {
+    this.userService.delete(listItem).subscribe(null, e => console.log(e));
+  }
+}
