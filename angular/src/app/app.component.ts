08ed3a2135b8127c64906cc8a949c3463bad04cf
Sebastian Springer <info@sebastian-springer.com>
2017-11-04 20:54:53 +0100

    FE: hide nav menu for login page

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, UrlSegment } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'todo-root',
@@ -7,5 +8,13 @@ import { ActivatedRoute, UrlSegment } from '@angular/router';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  hideNav() {}
+  public isNavHidden = false;
+  constructor(private router: Router) {
+    router.events
+      .filter(event => event instanceof NavigationEnd)
+      .map(() => this.router.routerState.snapshot)
+      .subscribe(event => {
+        this.isNavHidden = event.url === '/login';
+      });
+  }
 }
