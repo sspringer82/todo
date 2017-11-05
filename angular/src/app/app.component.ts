9564eff1709a2806d9d24d269b6625971d578f47
Sebastian Springer <info@sebastian-springer.com>
2017-11-05 17:22:37 +0100

    FE: hide users in navigation for non admins

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { LoginService } from './services/login.service';
 
 @Component({
   selector: 'todo-root',
@@ -9,12 +10,18 @@ import { Observable } from 'rxjs/Observable';
 })
 export class AppComponent {
   public isNavHidden = false;
-  constructor(private router: Router) {
+  public isAdmin = false;
+  constructor(private router: Router, private loginService: LoginService) {
     router.events
       .filter(event => event instanceof NavigationEnd)
       .map(() => this.router.routerState.snapshot)
       .subscribe(event => {
         this.isNavHidden = event.url === '/login';
+
+        if (!this.isNavHidden) {
+          const user = this.loginService.getUser();
+          this.isAdmin = !!(user && user.isAdmin);
+        }
       });
   }
 }
