6716d308ee0f6d1ea5db222f8c534cb192494496
Sebastian Springer <info@sebastian-springer.com>
2017-11-13 22:03:20 +0100

    FE: Description dialog

---
+++ angular/src/app/todo/components/description-dialog/description-dialog.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DescriptionDialogComponent } from './description-dialog.component';
+
+describe('DescriptionDialogComponent', () => {
+  let component: DescriptionDialogComponent;
+  let fixture: ComponentFixture<DescriptionDialogComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DescriptionDialogComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DescriptionDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular/src/app/todo/components/description-dialog/description-dialog.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Todo } from '../../models/todo';
+
+@Component({
+  selector: 'todo-description-dialog',
+  templateUrl: './description-dialog.component.html',
+  styleUrls: ['./description-dialog.component.css'],
+})
+export class DescriptionDialogComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<DescriptionDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Todo,
+  ) {}
+
+  ngOnInit() {}
+}
