d68bcf82298193b6e47c2af6f8fd33f932ec27d3
Sebastian Springer <info@sebastian-springer.com>
2017-11-18 18:36:38 +0100

    FE: moved routing guards

--- angular/src/app/user/user-routing.module.ts
+++ angular/src/app/user/user-routing.module.ts
@@ -4,24 +4,31 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
 import { FormComponent } from './components/form/form.component';
 import { AdminAuthGuard } from '../shared/services/admin-auth-guard.service';
+import { RegisterComponent } from './components/register/register.component';
 
 const listRoutes: Routes = [
   {
     path: 'user',
-    canActivate: [AdminAuthGuard],
     children: [
       {
         path: 'list',
         component: ListComponent,
+        canActivate: [AdminAuthGuard],
       },
       {
         path: 'form',
         component: FormComponent,
+        canActivate: [AdminAuthGuard],
       },
-
       {
         path: 'edit/:id',
         component: FormComponent,
+        canActivate: [AdminAuthGuard],
+      },
+
+      {
+        path: 'register',
+        component: RegisterComponent,
       },
     ],
   },
