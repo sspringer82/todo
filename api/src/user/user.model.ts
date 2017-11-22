cc52206f5883329ccc07a4379706da9c595e7008
Sebastian Springer <info@sebastian-springer.com>
2017-11-22 18:17:22 +0100

    BE: default list for new users

--- api/src/user/user.model.ts
+++ api/src/user/user.model.ts
@@ -2,6 +2,7 @@ import { Database, RunResult } from 'sqlite3';
 import { DbApi } from '../shared/db-api';
 import { User } from '../shared/user.type';
 import { PassThrough } from 'stream';
+import listModel from '../list/list.model';
 
 const db = new Database('db/database.sqlite3');
 const dbAPI = new DbApi<User>(db);
@@ -25,7 +26,19 @@ const userModel = {
       'INSERT INTO user (username, password, isActive, isAdmin) VALUES (?, ?, ?, ?)';
     return dbAPI
       .run(query, [user.username, user.password, user.isActive, user.isAdmin])
-      .then((data: RunResult) => ({ ...user, id: data.lastID }));
+      .then((data: RunResult) => {
+        return new Promise<User>((resolve, reject) => {
+          const newUser = { ...user, id: data.lastID };
+          listModel
+            .create(
+              { id: 0, title: 'default', owner: data.lastID },
+              data.lastID,
+            )
+            .then(() => {
+              resolve(newUser);
+            });
+        });
+      });
   },
   update(user: User): Promise<User> {
     let result;
