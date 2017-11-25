467fe328ec3cfec00de0a8d08397c5699d8a021b
Sebastian Springer <info@sebastian-springer.com>
2017-11-25 20:56:06 +0100

    react: stateful component

--- react/src/todo/list.component.js
+++ react/src/todo/list.component.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { ListItem } from './list-item.component';
 
-const todos = [
-  { id: 1, title: 'eat' },
-  { id: 2, title: 'sleep' },
-  { id: 3, title: 'code' },
-];
+export class List extends React.Component {
+  state = {
+    todos: [],
+  };
 
-export function List() {
-  return <ul>{todos.map(todo => <ListItem key={todo.id} todo={todo} />)}</ul>;
+  componentDidMount() {
+    Promise.resolve().then(() => {
+      this.setState(() => {
+        return {
+          todos: [
+            { id: 1, title: 'eat' },
+            { id: 2, title: 'sleep' },
+            { id: 3, title: 'code' },
+          ],
+        };
+      });
+    });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.todos.map(todo => <ListItem key={todo.id} todo={todo} />)}
+      </ul>
+    );
+  }
 }
