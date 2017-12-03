56dfde7a3f038e23df667505c0bfc71953670203
Sebastian Springer <info@sebastian-springer.com>
2017-12-03 15:32:01 +0100

    react: removed debugging statement

--- react/src/todo/components/list.component.js
+++ react/src/todo/components/list.component.js
@@ -3,7 +3,6 @@ import { ListItem } from './list-item.component';
 
 export class List extends React.Component {
   render() {
-    console.log(this.props.todos);
     return (
       <ul>
         {this.props.todos.map(todo => <ListItem key={todo.id} todo={todo} />)}
