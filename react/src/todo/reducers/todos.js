706d779cea8bf01476714f80aedf9a5f073e1485
Sebastian Springer <info@sebastian-springer.com>
2017-12-08 21:50:08 +0100

    react: add todo reducer

--- react/src/todo/reducers/todos.js
+++ react/src/todo/reducers/todos.js
@@ -9,6 +9,16 @@ const defaultState = {
 
 export const todos = (state = defaultState, action) => {
   switch (action.type) {
+    case 'ADD_TODO':
+      const id =
+        state.todos.reduce(
+          (prev, curr) => (curr.id > prev ? curr.id : prev),
+          0,
+        ) + 1;
+
+      return {
+        todos: [...state.todos, { ...action.payload.todo, id }],
+      };
     default:
       return {
         todos: state.todos,
