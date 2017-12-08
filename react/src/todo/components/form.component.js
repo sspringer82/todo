450f10fe7c7f835fd6164a69a2699bb0d260c52e
Sebastian Springer <info@sebastian-springer.com>
2017-12-08 07:12:56 +0100

    react: form

--- react/src/todo/components/form.component.js
+++ react/src/todo/components/form.component.js
@@ -1,3 +1,41 @@
 import React from 'react';
 
-export const Form = () => <div>Form works</div>;
+export class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: 'my fancy title',
+    };
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState(prev => {
+      return { ...prev, [name]: value };
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <form onSubmit={e => this.handleSubmit(e)}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            onChange={e => this.handleChange(e)}
+            value={this.state.title}
+          />
+        </div>
+        <button>submit</button>
+      </form>
+    );
+  }
+}
