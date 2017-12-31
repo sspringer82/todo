7b28cba4efb96f47e4dbf0cb517e142bafd7a3d0
Sebastian Springer <info@sebastian-springer.com>
2017-12-31 22:13:25 +0100

    react: do login request

--- react/src/login/epics/index.js
+++ react/src/login/epics/index.js
@@ -1,10 +1,24 @@
+import { ajax } from 'rxjs/observable/dom/ajax';
+
 import { ofType } from 'redux-observable';
-import { map } from 'rxjs/operators';
+import { mergeMap, map, catchError } from 'rxjs/operators';
 import { LOGIN } from '../actions/index';
 
 import { loginSuccess } from '../actions';
 import 'rxjs/add/observable/of';
 
 export const loginEpic = (action$, store) => {
-  return action$.pipe(ofType(LOGIN), map(() => loginSuccess('secret token')));
+  return action$.pipe(
+    ofType(LOGIN),
+    mergeMap(action =>
+      ajax
+        .post('/login', action.payload)
+        .pipe(
+          map(
+            response => loginSuccess(response),
+            catchError(e => console.log(e)),
+          ),
+        ),
+    ),
+  );
 };
