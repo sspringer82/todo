f3420de763ef08439e885efc62a6a97b4e954761
Sebastian Springer <info@sebastian-springer.com>
2017-12-31 22:13:15 +0100

    react: remove unused import

--- react/src/todo/index.js
+++ react/src/todo/index.js
@@ -1,6 +1,4 @@
 import { combineEpics } from 'redux-observable';
-import { combineReducers } from 'redux';
-
 import { todoEpic } from './epics/index';
 
 export const rootEpic = combineEpics(todoEpic);
