4a17b181e038b5bc74305d6ea8e2b79355d01321
Sebastian Springer <info@sebastian-springer.com>
2018-01-18 20:49:18 +0100

    BE: fix sequence recalculation

--- api/src/todo/todo.helper.ts
+++ api/src/todo/todo.helper.ts
@@ -4,11 +4,13 @@ import { Todo } from '../shared/todo.type';
 export const reorder = async (list: string, userId: number) => {
   const allTodos = await model.getAll();
   let sequence = 1;
-  allTodos.forEach(async (todo: Todo) => {
-    if (todo.sequence !== sequence) {
-      todo.sequence = sequence;
-      await model.update(todo, userId);
-    }
-    sequence++;
-  });
+  allTodos
+    .filter((todo: Todo) => !todo.archived && list === todo.list)
+    .forEach(async (todo: Todo) => {
+      if (todo.sequence !== sequence) {
+        todo.sequence = sequence;
+        await model.update(todo, userId);
+      }
+      sequence++;
+    });
 };
