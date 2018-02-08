7a79001ceefa02cf5793c6ac6ac98627721c3bbb
Sebastian Springer <info@sebastian-springer.com>
2018-02-08 07:41:23 +0100

    BE: remove debugging statement

--- api/src/todo/todo.helper.ts
+++ api/src/todo/todo.helper.ts
@@ -2,7 +2,6 @@ import { model } from './todo.model';
 import { Todo } from '../shared/todo.type';
 
 export const reorder = async (list: string, userId: number) => {
-  console.log('reordering');
   const allTodos = await model.getAll(list);
   let sequence = 0;
   allTodos.forEach(async (todo: Todo) => {
