95ce26d401c7a0d8c20b576ca953e0b5ec5fe6cd
Sebastian Springer <info@sebastian-springer.com>
2018-03-11 19:29:28 +0100

    BE: updated to use winston 3

--- api/src/shared/logger.ts
+++ api/src/shared/logger.ts
@@ -1,6 +1,7 @@
 import * as winston from 'winston';
 
-const logger = new winston.Logger({
+const logger = new winston.createLogger({
+  format: winston.format.simple(),
   transports: [
     new winston.transports.Console(),
     new winston.transports.File({ filename: 'logs/application.log' }),
