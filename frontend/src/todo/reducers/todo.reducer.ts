13f1b8fc01969cfedef9f2929a44be44a991976c
Sebastian Springer <info@sebastian-springer.com>
2019-11-25 11:46:05 +0100

    create, update, delete

--- frontend/src/todo/reducers/todo.reducer.ts
+++ frontend/src/todo/reducers/todo.reducer.ts
@@ -1,5 +1,9 @@
 import { Todo } from '../../shared/Todo';
-import { LOAD_TODOS_SUCCESS } from '../actions/todo.actions';
+import {
+  LOAD_TODOS_SUCCESS,
+  SAVE_TODO_SUCCESS,
+  DELETE_TODO_SUCCESS,
+} from '../actions/todo.actions';
 import update from 'immutability-helper';
 
 export interface State {
@@ -14,6 +18,20 @@ export default function(state: State = initialState, action: any): State {
   switch (action.type) {
     case LOAD_TODOS_SUCCESS:
       return update(state, { todos: { $set: action.payload } });
+    case SAVE_TODO_SUCCESS:
+      const index = state.todos.findIndex(
+        todo => todo.id === action.payload.id
+      );
+      if (index > -1) {
+        return update(state, { todos: { [index]: { $set: action.payload } } });
+      } else {
+        return update(state, { todos: { $push: [action.payload] } });
+      }
+    case DELETE_TODO_SUCCESS:
+      const deleteIndex = state.todos.findIndex(
+        todo => todo.id === action.payload.id
+      );
+      return update(state, { todos: { $splice: [[deleteIndex, 1]] } });
     default:
       return state;
   }
--- frontend/src/todo/sagas/todo.saga.ts
+++ frontend/src/todo/sagas/todo.saga.ts
@@ -1,7 +1,17 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { takeLatest, put } from '@redux-saga/core/effects';
 import { Todo } from '../../shared/Todo';
-import { loadTodosSuccessAction, LOAD_TODOS } from '../actions/todo.actions';
+import {
+  loadTodosSuccessAction,
+  LOAD_TODOS,
+  SAVE_TODO,
+  saveTodoAction,
+  saveTodoSuccessAction,
+  deleteTodoAction,
+  DELETE_TODO,
+  deleteTodoSuccessAction,
+} from '../actions/todo.actions';
+import { ActionType } from 'typesafe-actions';
 
 function* loadTodos() {
   const { data } = yield axios.get<Todo[]>(
@@ -10,6 +20,29 @@ function* loadTodos() {
   yield put(loadTodosSuccessAction(data));
 }
 
+function* save({ payload: todo }: ActionType<typeof saveTodoAction>) {
+  let response: AxiosResponse<Todo>;
+  if (todo.id) {
+    response = yield axios.put<Todo>(
+      `${process.env.REACT_APP_SERVER}/todos/${todo.id}`,
+      todo
+    );
+  } else {
+    response = yield axios.post<Todo>(
+      `${process.env.REACT_APP_SERVER}/todos/`,
+      todo
+    );
+  }
+  yield put(saveTodoSuccessAction(response.data));
+}
+
+function* remove({ payload: todo }: ActionType<typeof deleteTodoAction>) {
+  yield axios.delete(`${process.env.REACT_APP_SERVER}/todos/${todo.id}`);
+  yield put(deleteTodoSuccessAction(todo));
+}
+
 export default function* todoSaga() {
   yield takeLatest(LOAD_TODOS, loadTodos);
+  yield takeLatest(SAVE_TODO, save);
+  yield takeLatest(DELETE_TODO, remove);
 }
