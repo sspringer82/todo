b93d21426d85b1043e4f3276288f5edd49b0f3d8
Sebastian Springer <info@sebastian-springer.com>
2019-12-01 11:27:35 +0100

    use guards for endpoints

--- backend/src/todo/todo/todo.controller.ts
+++ backend/src/todo/todo/todo.controller.ts
@@ -6,30 +6,36 @@ import {
   Post,
   Delete,
   Param,
+  UseGuards,
 } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { Todo } from './todo.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('todo')
 export class TodoController {
   constructor(private todoService: TodoService) {}
 
   @Get()
+  @UseGuards(AuthGuard('jwt'))
   getAll() {
     return this.todoService.getAll();
   }
 
   @Post()
+  @UseGuards(AuthGuard('jwt'))
   create(@Body() todo: Todo) {
     return this.todoService.save(todo);
   }
 
   @Put(':id')
+  @UseGuards(AuthGuard('jwt'))
   update(@Body() todo: Todo) {
     return this.todoService.save(todo);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard('jwt'))
   remove(@Param('id') id: string) {
     this.todoService.remove(parseInt(id, 10));
   }
