6c93a0c173698a93dc946ac4a91a43ec8a6eac91
Sebastian Springer <info@sebastian-springer.com>
2019-12-20 16:50:50 +0100

    settings service

--- backend/src/settings/settings/settings.service.ts
+++ backend/src/settings/settings/settings.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Settings } from './settings.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class SettingsService {}
+export class SettingsService {
+  constructor(
+    @InjectRepository(Settings)
+    private readonly settingsRepository: Repository<Settings>
+  ) {}
+
+  getByUserId(id: number) {
+    return this.settingsRepository.findOne({ where: { userId: id } });
+  }
+
+  save(settings: Settings) {
+    return this.settingsRepository.save(settings);
+  }
+}
