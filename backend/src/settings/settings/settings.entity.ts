d4a0095ba72ae4cea77df7d63adb548c5ee8c00e
Sebastian Springer <info@sebastian-springer.com>
2019-12-20 16:50:39 +0100

    settings entity

--- backend/src/settings/settings/settings.entity.ts
+++ backend/src/settings/settings/settings.entity.ts
@@ -4,15 +4,27 @@ import {
   Column,
   OneToOne,
   JoinColumn,
+  ManyToOne,
 } from 'typeorm';
 import { User } from 'src/user/user/user.entity';
+import { List } from 'src/todo/list/list.entity';
 @Entity()
 export class Settings {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column('text')
-  name: string;
+  @Column({ default: 0 })
+  hideDone: boolean;
+
+  @Column({ default: 0 })
+  onlyStars: boolean;
+
+  @ManyToOne(
+    type => List,
+    list => list.todos,
+    { nullable: true, onDelete: 'SET NULL' }
+  )
+  list: List;
 
   @OneToOne(
     type => User,
