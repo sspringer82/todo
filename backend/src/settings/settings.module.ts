3035b91f59f0cf3c41ea2fe145db11d28ba38819
Sebastian Springer <info@sebastian-springer.com>
2019-12-20 16:50:57 +0100

    settings controller

--- backend/src/settings/settings.module.ts
+++ backend/src/settings/settings.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SettingsController } from './settings/settings.controller';
 import { SettingsService } from './settings/settings.service';
+import { Settings } from './settings/settings.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Settings])],
   controllers: [SettingsController],
-  providers: [SettingsService]
+  providers: [SettingsService],
 })
 export class SettingsModule {}
--- backend/src/settings/settings/settings.controller.ts
+++ backend/src/settings/settings/settings.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { SettingsService } from './settings.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('settings')
-export class SettingsController {}
+export class SettingsController {
+  constructor(private readonly settingsService: SettingsService) {}
+
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  async getAll(@Req() request) {
+    const result = await this.settingsService.getByUserId(request.user.id);
+    return result;
+  }
+}
