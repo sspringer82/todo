e3dbd9dead43362007987e33004f152d09a4b650
Sebastian Springer <info@sebastian-springer.com>
2020-01-04 07:17:12 +0100

    add local user table

--- frontend/src/db/db.ts
+++ frontend/src/db/db.ts
@@ -2,12 +2,14 @@ import Dexie from 'dexie';
 import { Settings } from '../shared/Settings';
 import { List } from '../shared/List';
 import { InputTypeTodo } from '../shared/Todo';
+import { User } from '../shared/User';
 
 export class TodoDatabase extends Dexie {
   settings: Dexie.Table<Settings, number> | undefined;
   list: Dexie.Table<List, number> | undefined;
   todo: Dexie.Table<InputTypeTodo, number> | undefined;
   changes: Dexie.Table<any, number> | undefined;
+  user: Dexie.Table<User, number> | undefined;
 
   constructor() {
     super('TodoDatabase');
@@ -16,6 +18,7 @@ export class TodoDatabase extends Dexie {
       list: 'id++, name',
       todo: 'id++, title',
       changes: 'id++',
+      user: 'id++',
     });
   }
 }
