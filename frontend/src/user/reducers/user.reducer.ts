97b31de281d2ddb7456de80f4abbf1c607783b67
Sebastian Springer <info@sebastian-springer.com>
2020-01-04 07:27:06 +0100

    make user info available offline

--- frontend/src/user/reducers/user.reducer.ts
+++ frontend/src/user/reducers/user.reducer.ts
@@ -1,6 +1,7 @@
 import { User } from '../../shared/User';
 import { LOAD_USERS_SUCCESS } from '../actions/user.actions';
 import update from 'immutability-helper';
+import db from '../../db/db';
 
 export interface State {
   users: User[];
@@ -13,6 +14,10 @@ const initialState: State = {
 export default function(state: State = initialState, action: any): State {
   switch (action.type) {
     case LOAD_USERS_SUCCESS:
+      if (navigator.onLine) {
+        db.table('user').clear();
+        db.table('user').bulkAdd(action.payload);
+      }
       return update(state, { users: { $set: action.payload } });
     default:
       return state;
--- frontend/src/user/sagas/user.saga.ts
+++ frontend/src/user/sagas/user.saga.ts
@@ -1,5 +1,6 @@
 import { takeLatest, put, select } from '@redux-saga/core/effects';
 import axios from 'axios';
+import db from '../../db/db';
 
 import { getToken } from '../../login/selectors/login.selector';
 import { User } from '../../shared/User';
@@ -7,14 +8,16 @@ import { loadUsersSuccessAction, LOAD_USERS } from '../actions/user.actions';
 
 function* loadUsers() {
   const token = yield select(getToken);
-  const { data: users } = yield axios.get<User[]>(
-    `${process.env.REACT_APP_SERVER}/user`,
-    {
+  let users: User[];
+  if (navigator.onLine) {
+    users = (yield axios.get<User[]>(`${process.env.REACT_APP_SERVER}/user`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
-    }
-  );
+    })).data;
+  } else {
+    users = yield db.table('user').toArray();
+  }
 
   yield put(loadUsersSuccessAction(users));
 }
