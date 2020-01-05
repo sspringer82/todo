031426dc35abd0106e13b883a837cd44f0ef777b
Sebastian Springer <info@sebastian-springer.com>
2020-01-05 16:35:01 +0100

    changes actions

---
+++ frontend/src/changes/actions/changes.actions.ts
@@ -0,0 +1,15 @@
+import { createAction } from 'typesafe-actions';
+import { Change } from '../../shared/Change';
+
+export const ADD_CHANGE = 'ADD_CHANGE';
+export type ADD_CHANGE = typeof ADD_CHANGE;
+
+export const APPLY_CHANGES = 'APPLY_CHANGES';
+export type APPLY_CHANGES = typeof APPLY_CHANGES;
+
+export const CLEAR_CHANGES = 'CLEAR_CHANGES';
+export type CLEAR_CHANGES = typeof CLEAR_CHANGES;
+
+export const addChangeAction = createAction(ADD_CHANGE)<Change>();
+export const applyChangesAction = createAction(APPLY_CHANGES)<void>();
+export const clearChangesAction = createAction(CLEAR_CHANGES)<void>();
