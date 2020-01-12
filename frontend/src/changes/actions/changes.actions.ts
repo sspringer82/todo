76b6c1889b70528d513319effd392048c934f0a0
Sebastian Springer <info@sebastian-springer.com>
2020-01-12 18:37:17 +0100

    connection state action

--- frontend/src/changes/actions/changes.actions.ts
+++ frontend/src/changes/actions/changes.actions.ts
@@ -10,6 +10,11 @@ export type APPLY_CHANGES = typeof APPLY_CHANGES;
 export const CLEAR_CHANGES = 'CLEAR_CHANGES';
 export type CLEAR_CHANGES = typeof CLEAR_CHANGES;
 
+export const CONNECTION_STATE = 'CONNECTION_STATE';
+export type CONNECTION_STATE = typeof CONNECTION_STATE;
+
 export const addChangeAction = createAction(ADD_CHANGE)<Change>();
 export const applyChangesAction = createAction(APPLY_CHANGES)<void>();
 export const clearChangesAction = createAction(CLEAR_CHANGES)<void>();
+
+export const connectionStateAction = createAction(CONNECTION_STATE)<boolean>();
