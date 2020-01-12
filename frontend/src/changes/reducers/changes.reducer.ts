a7a112bbfd2a779947a3fd817f4958edcccadf00
Sebastian Springer <info@sebastian-springer.com>
2020-01-12 18:50:09 +0100

    fix changes reducer action type

--- frontend/src/changes/reducers/changes.reducer.ts
+++ frontend/src/changes/reducers/changes.reducer.ts
@@ -1,5 +1,9 @@
-import { CONNECTION_STATE } from '../actions/changes.actions';
+import {
+  CONNECTION_STATE,
+  connectionStateAction,
+} from '../actions/changes.actions';
 import update from 'immutability-helper';
+import { ActionType } from 'typesafe-actions';
 
 export interface State {
   connection: boolean;
@@ -9,7 +13,10 @@ const initialState: State = {
   connection: true,
 };
 
-export default function(state: State = initialState, action: any): State {
+export default function(
+  state: State = initialState,
+  action: ActionType<typeof connectionStateAction>
+): State {
   switch (action.type) {
     case CONNECTION_STATE:
       return update(state, { connection: { $set: action.payload } });
