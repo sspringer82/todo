164b693bd01d482b3d5e43fc8a7b0e52d3721588
Sebastian Springer <info@sebastian-springer.com>
2020-01-19 21:37:16 +0100

    online and offline handling

--- frontend/src/changes/actions/changes.actions.ts
+++ frontend/src/changes/actions/changes.actions.ts
@@ -10,11 +10,15 @@ export type APPLY_CHANGES = typeof APPLY_CHANGES;
 export const CLEAR_CHANGES = 'CLEAR_CHANGES';
 export type CLEAR_CHANGES = typeof CLEAR_CHANGES;
 
-export const CONNECTION_STATE = 'CONNECTION_STATE';
-export type CONNECTION_STATE = typeof CONNECTION_STATE;
+export const ONLINE = 'ONLINE';
+export type ONLINE = typeof ONLINE;
+
+export const OFFLINE = 'OFFLINE';
+export type OFFLINE = typeof OFFLINE;
 
 export const addChangeAction = createAction(ADD_CHANGE)<Change>();
 export const applyChangesAction = createAction(APPLY_CHANGES)<void>();
 export const clearChangesAction = createAction(CLEAR_CHANGES)<void>();
 
-export const connectionStateAction = createAction(CONNECTION_STATE)<boolean>();
+export const onlineAction = createAction(ONLINE)<void>();
+export const offlineAction = createAction(OFFLINE)<void>();
--- frontend/src/changes/reducers/changes.reducer.ts
+++ frontend/src/changes/reducers/changes.reducer.ts
@@ -1,25 +1,29 @@
 import {
-  CONNECTION_STATE,
-  connectionStateAction,
+  ONLINE,
+  OFFLINE,
+  onlineAction,
+  offlineAction,
 } from '../actions/changes.actions';
 import update from 'immutability-helper';
 import { ActionType } from 'typesafe-actions';
 
 export interface State {
-  connection: boolean;
+  online: boolean;
 }
 
 const initialState: State = {
-  connection: navigator.onLine,
+  online: navigator.onLine,
 };
 
 export default function(
   state: State = initialState,
-  action: ActionType<typeof connectionStateAction>
+  action: ActionType<typeof onlineAction | typeof offlineAction>
 ): State {
   switch (action.type) {
-    case CONNECTION_STATE:
-      return update(state, { connection: { $set: action.payload } });
+    case ONLINE:
+      return update(state, { online: { $set: true } });
+    case OFFLINE:
+      return update(state, { online: { $set: false } });
     default:
       return state;
   }
