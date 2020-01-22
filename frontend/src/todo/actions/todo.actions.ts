1748a559e583ca807832b23d8378605a25ec71ef
Sebastian Springer <info@sebastian-springer.com>
2020-01-22 22:19:11 +0100

    handle offline todo

--- frontend/src/todo/actions/todo.actions.ts
+++ frontend/src/todo/actions/todo.actions.ts
@@ -4,8 +4,14 @@ import { Todo, InputTypeTodo } from '../../shared/Todo';
 export const LOAD_TODOS = 'LOAD_TODOS';
 export type LOAD_TODOS = typeof LOAD_TODOS;
 
+export const LOAD_TODOS_OFFLINE = 'LOAD_TODOS';
+export type LOAD_TODOS_OFFLINE = typeof LOAD_TODOS;
+
 export const LOAD_TODOS_SUCCESS = 'LOAD_TODOS_SUCCESS';
 export type LOAD_TODOS_SUCCESS = typeof LOAD_TODOS_SUCCESS;
+
+export const LOAD_TODOS_ERROR = 'LOAD_TODOS_ERROR';
+export type LOAD_TODOS_ERROR = typeof LOAD_TODOS_ERROR;
 
 export const SAVE_TODO = 'SAVE_TODO';
 export type SAVE_TODO = typeof SAVE_TODO;
@@ -22,9 +28,11 @@ export type DELETE_TODO_SUCCESS = typeof DELETE_TODO_SUCCESS;
 export const SEARCH = 'SEARCH';
 
 export const loadTodosAction = createAction(LOAD_TODOS)<void>();
+export const loadTodosOfflineAction = createAction(LOAD_TODOS_OFFLINE)<void>();
 export const loadTodosSuccessAction = createAction(LOAD_TODOS_SUCCESS)<
   Todo[]
 >();
+export const loadTodosErrorAction = createAction(LOAD_TODOS_ERROR)<string>();
 
 export const saveTodoAction = createAction(SAVE_TODO)<InputTypeTodo>();
 export const saveTodoSuccessAction = createAction(SAVE_TODO_SUCCESS)<Todo>();
