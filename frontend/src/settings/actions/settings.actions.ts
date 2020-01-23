0928b8ba6968a8bca11edf8d1a693d7dc800df46
Sebastian Springer <info@sebastian-springer.com>
2020-01-23 21:57:08 +0100

    settings saga

--- frontend/src/settings/actions/settings.actions.ts
+++ frontend/src/settings/actions/settings.actions.ts
@@ -10,8 +10,23 @@ export type LOAD_SETTINGS_SUCCESS = typeof LOAD_SETTINGS_SUCCESS;
 export const SAVE_SETTINGS = 'SAVE_SETTINGS';
 export type SAVE_SETTINGS = typeof SAVE_SETTINGS;
 
+export const CREATE_SETTINGS = 'CREATE_SETTINGS';
+export type CREATE_SETTINGS = typeof CREATE_SETTINGS;
+
+export const UPDATE_SETTINGS = 'UPDATE_SETTINGS';
+export type UPDATE_SETTINGS = typeof UPDATE_SETTINGS;
+
+export const CREATE_SETTINGS_OFFLINE = 'CREATE_SETTINGS_OFFLINE';
+export type CREATE_SETTINGS_OFFLINE = typeof CREATE_SETTINGS_OFFLINE;
+
+export const SAVE_SETTINGS_ERROR = 'SAVE_SETTINGS_ERROR';
+export type SAVE_SETTINGS_ERROR = typeof SAVE_SETTINGS_ERROR;
+
 export const SAVE_SETTINGS_SUCCESS = 'SAVE_SETTINGS_SUCCESS';
 export type SAVE_SETTINGS_SUCCESS = typeof SAVE_SETTINGS_SUCCESS;
+
+export const UPDATE_SETTINGS_OFFLINE = 'UPDATE_SETTINGS_OFFLINE';
+export type UPDATE_SETTINGS_OFFLINE = typeof UPDATE_SETTINGS_OFFLINE;
 
 export const loadSettingsAction = createAction(LOAD_SETTINGS)<void>();
 export const loadSettingsSuccessAction = createAction(LOAD_SETTINGS_SUCCESS)<
@@ -19,6 +34,19 @@ export const loadSettingsSuccessAction = createAction(LOAD_SETTINGS_SUCCESS)<
 >();
 
 export const saveSettingsAction = createAction(SAVE_SETTINGS)<Settings>();
+export const createSettingsAction = createAction(CREATE_SETTINGS)<Settings>();
+export const createSettingsOfflineAction = createAction(
+  CREATE_SETTINGS_OFFLINE
+)<Settings>();
+export const saveSettingsErrorAction = createAction(SAVE_SETTINGS_ERROR)<
+  string
+>();
+
+export const updateSettingsAction = createAction(UPDATE_SETTINGS)<Settings>();
+export const updateSettingsOfflineAction = createAction(
+  UPDATE_SETTINGS_OFFLINE
+)<Settings>();
+
 export const saveSettingsSuccessAction = createAction(SAVE_SETTINGS_SUCCESS)<
   Settings
 >();
--- frontend/src/settings/sagas/settings.saga.ts
+++ frontend/src/settings/sagas/settings.saga.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { takeLatest, put, select } from '@redux-saga/core/effects';
+import { takeLatest, put, select, all } from '@redux-saga/core/effects';
 import { getToken } from '../../login/selectors/login.selector';
 import {
   LOAD_SETTINGS,
@@ -8,11 +8,24 @@ import {
   loadSettingsSuccessAction,
   saveSettingsAction,
   saveSettingsSuccessAction,
+  createSettingsAction,
+  saveSettingsErrorAction,
+  createSettingsOfflineAction,
+  CREATE_SETTINGS,
+  CREATE_SETTINGS_OFFLINE,
+  updateSettingsAction,
+  UPDATE_SETTINGS,
+  UPDATE_SETTINGS_OFFLINE,
+  updateSettingsOfflineAction,
 } from '../actions/settings.actions';
 import { Settings } from '../../shared/Settings';
 import { ActionType } from 'typesafe-actions';
 import db from '../../db/db';
 import update from 'immutability-helper';
+import {
+  onlineAction,
+  addChangeAction,
+} from '../../changes/actions/changes.actions';
 
 function* loadSettings() {
   let settings: Settings;
@@ -32,7 +45,7 @@ function* loadSettings() {
   yield put(loadSettingsSuccessAction(settings));
 }
 
-function* save({ payload: settings }: ActionType<typeof saveSettingsAction>) {
+/*function* save({ payload: settings }: ActionType<typeof saveSettingsAction>) {
   const token = yield select(getToken);
   let responseSettings: Settings;
   if (settings.id) {
@@ -67,9 +80,97 @@ function* save({ payload: settings }: ActionType<typeof saveSettingsAction>) {
     }
   }
   yield put(saveSettingsSuccessAction(responseSettings));
+}*/
+
+function* createSettings({
+  payload: settings,
+}: ActionType<typeof createSettingsAction>) {
+  try {
+    const token = yield select(getToken);
+    const responseSettings = (yield axios.put<Settings>(
+      `${process.env.REACT_APP_SERVER}/settings/${settings.id}`,
+      settings,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )).data;
+    yield all([
+      put(onlineAction()),
+      put(saveSettingsSuccessAction(responseSettings)),
+    ]);
+  } catch (e) {
+    if (e.message === 'Network Error') {
+      put(createSettingsOfflineAction(settings));
+    } else {
+      put(saveSettingsErrorAction(e.message));
+    }
+  }
+}
+
+function* updateSettings({
+  payload: settings,
+}: ActionType<typeof updateSettingsAction>) {
+  try {
+    const token = yield select(getToken);
+    const responseSettings = (yield axios.put<Settings>(
+      `${process.env.REACT_APP_SERVER}/settings/${settings.id}`,
+      settings,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )).data;
+    yield all([
+      put(onlineAction()),
+      put(saveSettingsSuccessAction(responseSettings)),
+    ]);
+  } catch (e) {
+    if (e.message === 'Network Error') {
+      put(updateSettingsOfflineAction(settings));
+    } else {
+      put(saveSettingsErrorAction(e.message));
+    }
+  }
+}
+
+function* createOffline(
+  action: ActionType<typeof createSettingsOfflineAction>
+) {
+  const id = yield db.table('settings').add(action.payload);
+  const responseSettings = update(action.payload, { id: { $set: id } });
+  yield all([
+    put(addChangeAction({ action })),
+    put(saveSettingsSuccessAction(responseSettings)),
+  ]);
+}
+
+function* updateOffline(
+  action: ActionType<typeof createSettingsOfflineAction>
+) {
+  yield db.table('settings').update(action.payload.id, action.payload);
+  const responseSettings = action.payload;
+  yield all([
+    put(addChangeAction({ action })),
+    put(saveSettingsSuccessAction(responseSettings)),
+  ]);
+}
+
+function* save({ payload: settings }: ActionType<typeof saveSettingsAction>) {
+  if (settings.id) {
+    yield put(updateSettingsAction(settings));
+  } else {
+    yield put(createSettingsAction(settings));
+  }
 }
 
 export default function* todoSaga() {
   yield takeLatest(LOAD_SETTINGS, loadSettings);
   yield takeLatest(SAVE_SETTINGS, save);
+  yield takeLatest(CREATE_SETTINGS, createSettings);
+  yield takeLatest(CREATE_SETTINGS_OFFLINE, createOffline);
+  yield takeLatest(UPDATE_SETTINGS, updateSettings);
+  yield takeLatest(UPDATE_SETTINGS_OFFLINE, updateOffline);
 }
