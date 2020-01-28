afda199df82e6eb2d4ddc92ef9692b43477a0e96
Sebastian Springer <info@sebastian-springer.com>
2020-01-28 21:56:36 +0100

    update list

--- frontend/src/list/sagas/list.saga.ts
+++ frontend/src/list/sagas/list.saga.ts
@@ -7,67 +7,124 @@ import {
   deleteListAction,
   deleteListSuccessAction,
   DELETE_LIST,
+  LOAD_LISTS_OFFLINE,
+  loadListsOfflineAction,
+  loadListsErrorAction,
+  createListAction,
+  updateListAction,
+  CREATE_LIST,
+  CREATE_LIST_OFFLINE,
+  UPDATE_LIST,
+  UPDATE_LIST_OFFLINE,
+  createListOfflineAction,
+  saveListErrorAction,
+  updateListOfflineAction,
 } from '../actions/list.actions';
-import { takeLatest, put, select } from '@redux-saga/core/effects';
+import { takeLatest, put, select, all } from '@redux-saga/core/effects';
 import axios from 'axios';
 import { getToken } from '../../login/selectors/login.selector';
 import { List } from '../../shared/List';
 import { ActionType } from 'typesafe-actions';
 import db from '../../db/db';
 import update from 'immutability-helper';
+import {
+  onlineAction,
+  addChangeAction,
+} from '../../changes/actions/changes.actions';
+import isNetworkError from '../../shared/helpers/isNetworkError';
 
 function* loadLists() {
-  let lists: List[] = [];
-
-  if (navigator.onLine) {
+  try {
     const token = yield select(getToken);
-    lists = (yield axios.get<List[]>(`${process.env.REACT_APP_SERVER}/list`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })).data;
-  } else {
-    lists = yield db.table('list').toArray();
+    const lists = (yield axios.get<List[]>(
+      `${process.env.REACT_APP_SERVER}/list`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )).data;
+    yield all([put(onlineAction()), put(loadListsSuccessAction(lists))]);
+  } catch (e) {
+    if (isNetworkError(e)) {
+      yield put(loadListsOfflineAction());
+    } else {
+      yield put(loadListsErrorAction(e.message));
+    }
   }
+}
 
-  yield put(loadListsSuccessAction(lists));
+function* loadOffline() {
+  const lists = yield db.table('list').toArray();
+  put(loadListsSuccessAction(lists));
 }
 
 function* save({ payload: list }: ActionType<typeof saveListAction>) {
-  const token = yield select(getToken);
-  let responseList: List;
   if (list.id) {
-    if (navigator.onLine) {
-      responseList = (yield axios.put<List>(
-        `${process.env.REACT_APP_SERVER}/list/${list.id}`,
-        list,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      )).data;
-    } else {
-      yield db.table('list').update(list.id, list);
-      responseList = list as List;
-    }
+    yield put(updateListAction(list as List));
   } else {
-    if (navigator.onLine) {
-      responseList = (yield axios.post<List>(
-        `${process.env.REACT_APP_SERVER}/list/`,
-        list,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      )).data;
+    yield put(createListAction(list));
+  }
+}
+
+function* createOnline({ payload: list }: ActionType<typeof createListAction>) {
+  try {
+    const token = yield select(getToken);
+    const responseList = (yield axios.post<List>(
+      `${process.env.REACT_APP_SERVER}/list/`,
+      list,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )).data;
+    yield all([put(onlineAction()), put(saveListSuccessAction(responseList))]);
+  } catch (e) {
+    if (isNetworkError(e)) {
+      yield put(createListOfflineAction(list));
     } else {
-      const id = yield db.table('list').add(list);
-      responseList = update(list, { id: { $set: id } }) as List;
+      yield put(saveListErrorAction(e.message));
     }
   }
-  yield put(saveListSuccessAction(responseList));
+}
+
+function* createOffline(action: ActionType<typeof createListOfflineAction>) {
+  const id = yield db.table('list').add(action.payload);
+  const responseList = update(action.payload, { id: { $set: id } }) as List;
+  yield all([
+    put(addChangeAction({ action })),
+    put(saveListSuccessAction(responseList)),
+  ]);
+}
+
+function* updateOnline({ payload: list }: ActionType<typeof updateListAction>) {
+  try {
+    const token = yield select(getToken);
+    const responseList = (yield axios.put<List>(
+      `${process.env.REACT_APP_SERVER}/list/${list.id}`,
+      list,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )).data;
+    yield all([put(onlineAction()), put(saveListSuccessAction(responseList))]);
+  } catch (e) {
+    if (isNetworkError(e)) {
+      yield put(createListOfflineAction(list));
+    } else {
+      yield put(saveListErrorAction(e.message));
+    }
+  }
+}
+function* updateOffline(action: ActionType<typeof updateListOfflineAction>) {
+  db.table('list').update(action.payload.id, action.payload);
+  yield all([
+    put(addChangeAction({ action })),
+    put(saveListSuccessAction(action.payload)),
+  ]);
 }
 
 function* remove({ payload: list }: ActionType<typeof deleteListAction>) {
@@ -87,5 +144,10 @@ function* remove({ payload: list }: ActionType<typeof deleteListAction>) {
 export default function* todoSaga() {
   yield takeLatest(LOAD_LISTS, loadLists);
   yield takeLatest(SAVE_LIST, save);
+  yield takeLatest(CREATE_LIST, createOnline);
+  yield takeLatest(CREATE_LIST_OFFLINE, createOffline);
+  yield takeLatest(UPDATE_LIST, updateOnline);
+  yield takeLatest(UPDATE_LIST_OFFLINE, updateOffline);
   yield takeLatest(DELETE_LIST, remove);
+  yield takeLatest(LOAD_LISTS_OFFLINE, loadOffline);
 }
