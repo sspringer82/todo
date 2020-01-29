38f70d7da70a6e2674da6e2d91fa956844f466b4
Sebastian Springer <info@sebastian-springer.com>
2020-01-29 20:43:12 +0100

    backend cleanup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
@@ -13,7 +11,5 @@ import { SettingsModule } from './settings/settings.module';
     UserModule,
     SettingsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
