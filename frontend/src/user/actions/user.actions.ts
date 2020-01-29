4da76cf281ea96e42890fc7f411c19e668c3cad1
Sebastian Springer <info@sebastian-springer.com>
2020-01-29 20:44:02 +0100

    user handling

--- frontend/src/user/actions/user.actions.ts
+++ frontend/src/user/actions/user.actions.ts
@@ -4,10 +4,18 @@ import { User } from '../../shared/User';
 export const LOAD_USERS = 'LOAD_USERS';
 export type LOAD_USERS = typeof LOAD_USERS;
 
+export const LOAD_USERS_OFFLINE = 'LOAD_USERS_OFFLINE';
+export type LOAD_USERS_OFFLINE = typeof LOAD_USERS_OFFLINE;
+
+export const LOAD_USERS_ERROR = 'LOAD_USERS_ERROR';
+export type LOAD_USERS_ERROR = typeof LOAD_USERS_ERROR;
+
 export const LOAD_USERS_SUCCESS = 'LOAD_USERS_SUCCESS';
 export type LOAD_USERS_SUCCESS = typeof LOAD_USERS_SUCCESS;
 
 export const loadUsersAction = createAction(LOAD_USERS)<void>();
+export const loadUsersOfflineAction = createAction(LOAD_USERS_OFFLINE)<void>();
+export const loadUsersErrorAction = createAction(LOAD_USERS_ERROR)<string>();
 export const loadUsersSuccessAction = createAction(LOAD_USERS_SUCCESS)<
   User[]
 >();
--- frontend/src/user/sagas/user.saga.ts
+++ frontend/src/user/sagas/user.saga.ts
@@ -1,27 +1,46 @@
-import { takeLatest, put, select } from '@redux-saga/core/effects';
+import { takeLatest, put, select, all } from '@redux-saga/core/effects';
 import axios from 'axios';
 import db from '../../db/db';
 
 import { getToken } from '../../login/selectors/login.selector';
 import { User } from '../../shared/User';
-import { loadUsersSuccessAction, LOAD_USERS } from '../actions/user.actions';
+import {
+  loadUsersSuccessAction,
+  LOAD_USERS,
+  LOAD_USERS_OFFLINE,
+  loadUsersOfflineAction,
+  loadUsersErrorAction,
+} from '../actions/user.actions';
+import isNetworkError from '../../shared/helpers/isNetworkError';
+import { onlineAction } from '../../changes/actions/changes.actions';
 
 function* loadUsers() {
-  const token = yield select(getToken);
-  let users: User[];
-  if (navigator.onLine) {
-    users = (yield axios.get<User[]>(`${process.env.REACT_APP_SERVER}/user`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })).data;
-  } else {
-    users = yield db.table('user').toArray();
+  try {
+    const token = yield select(getToken);
+    const users = (yield axios.get<User[]>(
+      `${process.env.REACT_APP_SERVER}/user`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )).data;
+    yield all([put(onlineAction()), put(loadUsersSuccessAction(users))]);
+  } catch (e) {
+    if (isNetworkError(e)) {
+      yield put(loadUsersOfflineAction());
+    } else {
+      yield put(loadUsersErrorAction(e.message));
+    }
   }
+}
 
+function* loadOffline() {
+  const users = yield db.table('user').toArray();
   yield put(loadUsersSuccessAction(users));
 }
 
 export default function* todoSaga() {
   yield takeLatest(LOAD_USERS, loadUsers);
+  yield takeLatest(LOAD_USERS_OFFLINE, loadOffline);
 }
