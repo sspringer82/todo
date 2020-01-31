02181d1076740f32afbcc11df89638a2cea9909a
Sebastian Springer <info@sebastian-springer.com>
2020-01-31 07:05:27 +0100

    added types to change interface

--- frontend/src/shared/Change.ts
+++ frontend/src/shared/Change.ts
@@ -1,4 +1,26 @@
+import { ActionType } from 'typesafe-actions';
+import {
+  createTodoOfflineAction,
+  updateTodoOfflineAction,
+} from '../todo/actions/todo.actions';
+import {
+  createListOfflineAction,
+  updateListOfflineAction,
+} from '../list/actions/list.actions';
+import { createSettingsOfflineAction } from '../settings/actions/settings.actions';
+import {
+  createSubtaskOfflineAction,
+  updateSubtaskOfflineAction,
+} from '../todo/actions/subtask.actions';
+
 export interface Change {
-  // @todo specify the various action types here
-  action: any;
+  action:
+    | ActionType<typeof createListOfflineAction>
+    | ActionType<typeof updateListOfflineAction>
+    | ActionType<typeof createSettingsOfflineAction>
+    | ActionType<typeof createSettingsOfflineAction>
+    | ActionType<typeof createSubtaskOfflineAction>
+    | ActionType<typeof updateSubtaskOfflineAction>
+    | ActionType<typeof createTodoOfflineAction>
+    | ActionType<typeof updateTodoOfflineAction>;
 }
