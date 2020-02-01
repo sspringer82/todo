8ddd14214aad689c175bd766efeae3310a6323b7
Sebastian Springer <info@sebastian-springer.com>
2020-02-01 18:38:23 +0100

    changes saga

--- frontend/src/changes/sagas/changes.saga.ts
+++ frontend/src/changes/sagas/changes.saga.ts
@@ -5,17 +5,29 @@ import {
   CLEAR_CHANGES,
   ONLINE,
 } from '../actions/changes.actions';
+import axios from 'axios';
 
-import { takeLatest } from '@redux-saga/core/effects';
+import { takeLatest, select, put } from '@redux-saga/core/effects';
 import db from '../../db/db';
+import { getToken } from '../../login/selectors/login.selector';
 
 function* addChange({ payload: change }: ActionType<typeof addChangeAction>) {
   yield db.table('changes').add(change);
 }
 
-function* applyChanges() {}
+function* applyChanges() {
+  const changes = yield db.table('changes').toArray();
+  yield axios.post(`${process.env.REACT_APP_SERVER}/changes/`, changes, {
+    headers: {
+      Authorization: `Bearer ${yield select(getToken)}`,
+    },
+  });
+  yield put(clearChanges());
+}
 
-function* clearChanges() {}
+function* clearChanges() {
+  db.table('changes').clear();
+}
 
 export default function* changesSaga() {
   yield takeLatest(ONLINE, applyChanges);
