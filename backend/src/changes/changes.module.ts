93385db4c8f553b33272cb74d2ece4d86188a651
Sebastian Springer <info@sebastian-springer.com>
2020-02-02 18:56:35 +0100

    import services to changes module

--- backend/src/changes/changes.module.ts
+++ backend/src/changes/changes.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ChangesController } from './changes.controller';
 import { ChangesService } from './changes.service';
+import { TodoModule } from 'src/todo/todo.module';
+import { SettingsModule } from 'src/settings/settings.module';
 
 @Module({
   controllers: [ChangesController],
   providers: [ChangesService],
+  imports: [TodoModule, SettingsModule],
 })
 export class ChangesModule {}
