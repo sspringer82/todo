2a88c34f4a23be04d80bd05ccf67b99339e94b0a
Sebastian Springer <info@sebastian-springer.com>
2020-02-22 21:00:30 +0100

    action to fetch initial data

--- frontend/src/login/actions/login.actions.ts
+++ frontend/src/login/actions/login.actions.ts
@@ -1,4 +1,4 @@
-import { createAsyncAction } from 'typesafe-actions';
+import { createAsyncAction, createAction } from 'typesafe-actions';
 import { Login } from '../../shared/User';
 
 export const LOGIN = 'LOGIN';
@@ -10,8 +10,13 @@ export type LOGIN_SUCCESS = typeof LOGIN_SUCCESS;
 export const LOGIN_ERROR = 'LOGIN_ERROR';
 export type LOGIN_ERROR = typeof LOGIN_ERROR;
 
+export const INITIAL_DATA = 'INITIAL_DATA';
+export type INITIAL_DATA = typeof INITIAL_DATA;
+
 export const loginAction = createAsyncAction(LOGIN, LOGIN_SUCCESS, LOGIN_ERROR)<
   Login,
   string,
   string
 >();
+
+export const initialDataAction = createAction(INITIAL_DATA)<void>();
