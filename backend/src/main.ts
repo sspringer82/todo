5c457a4870ac193e0d124deb921a095914ff3008
Sebastian Springer <info@sebastian-springer.com>
2020-08-17 21:25:14 +0200

    increase body limit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.enableCors();
 
   const options = new DocumentBuilder()
