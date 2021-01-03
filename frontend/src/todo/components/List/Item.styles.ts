6c47a8413fbd1f3cf6f142b7b6121c4b76bb9834
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-01-03 22:15:13 +0100

    fix layout problem with overflow:hidden

--- frontend/src/todo/components/List/Item.styles.ts
+++ frontend/src/todo/components/List/Item.styles.ts
@@ -4,7 +4,6 @@ import MUIEditIcon from '@material-ui/icons/Edit';
 export const MenuContainer = styled.div`
   transition: width 0.2s ease-in-out;
   overflow: hidden;
-  padding-right: 5px;
   white-space: nowrap;
 `;
 
