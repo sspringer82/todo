08a76df87326854575ab05dd0595e1685e8b94f1
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-03-29 07:22:18 +0200

    backend

--- src/List.tsx
+++ src/List.tsx
@@ -5,7 +5,11 @@ import { useTodo } from "./TodoContext";
 const List: React.FC = () => {
   const [todos, setTodos] = useTodo();
   useEffect(() => {
-    setTodos([{id: 1, title: 'Get up', done: true},{id: 2, title: 'Eat', done: false}, {id: 3, title: 'Sleep', done: false}])
+    (async () => {
+      const request = await fetch('http://localhost:3001/todo');
+      const data = await request.json();
+      setTodos(data);
+    })();
   }, [setTodos]);
   return <div>
     {todos.map(todo => <ListItem todo={todo} />)}
