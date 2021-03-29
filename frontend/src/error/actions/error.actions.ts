809f1a9c3c219007176d88f6a2e648014de033e0
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-03-29 06:06:06 +0200

    cleanup

--- frontend/src/error/actions/error.actions.ts
+++
@@ -1,6 +0,0 @@
-import { createAction } from 'typesafe-actions';
-
-export const ACTIVATE_ERROR = 'ACTIVATE_ERROR';
-export type ACTIVATE_ERROR = typeof ACTIVATE_ERROR;
-
-export const activateErrorAction = createAction(ACTIVATE_ERROR)<void>();
--- frontend/src/user/selectors/user.selector.ts
+++
@@ -1,5 +0,0 @@
-import { AppState } from '../../reducers/rootReducer';
-
-export function getUsers(state: AppState) {
-  return state.user.users;
-}
