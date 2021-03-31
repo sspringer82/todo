fb5842cae04dd1c2eeea534b36e701202f3e1e2d
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-03-31 07:59:22 +0200

    toggle status

--- src/list/ListItem.tsx
+++ src/list/ListItem.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import { Todo } from '../Todo';
+import React from "react";
+import { Todo } from "../Todo";
 
 type Props = {
-    todo: Todo,
-    onDelete: (id:number) => void,
-}
+  todo: Todo;
+  onDelete: (id: number) => void;
+  onStatusToggle: (todo: Todo) => void;
+};
 
-const ListItem: React.FC<Props> = ({todo, onDelete}) => {
-    return <div>
-        <div>{todo.title}</div>
-        <button onClick={() => onDelete(todo.id)}>delete</button>
+const ListItem: React.FC<Props> = ({ todo, onDelete, onStatusToggle }) => {
+  return (
+    <div>
+      <div>{todo.title}</div>
+      <div>
+        <button onClick={() => onStatusToggle(todo)}>{todo.done ? "✔" : "❌"}</button>
+      </div>
+      <button onClick={() => onDelete(todo.id)}>delete</button>
     </div>
-}
+  );
+};
 
 export default ListItem;
