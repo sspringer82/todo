4ad3a31992da0164374de63cd75e5b4669e9e09e
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-04-02 12:31:20 +0200

    support subtasks

---
+++ src/Subtask/Subtask.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Subtask } from '../Todo';
+
+type Props = {
+  subtasks: Subtask[]
+}
+
+const Subtask: React.FC = () => {
+  // list subtasks
+  // inline edit
+  // create
+  return <div>Subtask works</div>;
+}
+
+export default Subtask;
--- src/Todo.ts
+++ src/Todo.ts
@@ -3,9 +3,14 @@ export type Todo = {
     title: string,
     done: boolean,
     comment: string,
+    subtask?: Subtask[]
 };
 
 export type TodoInput = {id?: number} & Omit<Todo, 'id'>;
+
+export type Subtask = Omit<Todo, 'comment' | 'subtask'>
+
+export type SubtaskInput = {id?: number} & Omit<Subtask, 'id'>
 
 export const initialTodo:TodoInput = {
     title: '',
