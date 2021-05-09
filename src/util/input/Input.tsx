4b558b1e170f17328e739175dcbf6153a947399a
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-05-09 09:38:43 +0200

    fixed input

--- src/util/input/Input.tsx
+++ src/util/input/Input.tsx
@@ -1,7 +1,7 @@
 import { colors } from '../../colors';
 
-const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = () => {
-  return <input style={{borderColor: colors.active}} className="border bg-black py-1 focus:outline-none"/>
+const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = (props) => {
+  return <input style={{borderColor: colors.active}} className="border bg-black py-1 focus:outline-none" {...props} />
 }
 
 export default Input;
