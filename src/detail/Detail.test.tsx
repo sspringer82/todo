995497925d1af793cf80db631e38d0286997b127
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-05-16 09:55:05 +0200

    Detail test toggle status

--- src/detail/Detail.test.tsx
+++ src/detail/Detail.test.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from 'react-router-dom';
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import Detail, { Props } from "./Detail";
 
 describe("Detail", () => {
@@ -13,42 +13,59 @@ describe("Detail", () => {
   });
 
   it("should render details of a todo", async () => {
-    const {getByTestId} = render(<Detail {...props} />);
-    const title = getByTestId('detail-title');
-    const comment = getByTestId('detail-comment');
-    const done = getByTestId('done-done');
-    const noSubtasks = getByTestId('detail-nosubtasks');
-    expect(title).toHaveTextContent('New Todo');
-    expect(comment).toHaveTextContent('Todo Comment');
+    const { getByTestId } = render(<Detail {...props} />);
+    const title = getByTestId("detail-title");
+    const comment = getByTestId("detail-comment");
+    const done = getByTestId("done-done");
+    const noSubtasks = getByTestId("detail-nosubtasks");
+    expect(title).toHaveTextContent("New Todo");
+    expect(comment).toHaveTextContent("Todo Comment");
     expect(done).toBeInTheDocument();
     expect(noSubtasks).toBeInTheDocument();
   });
   it("should render details of a todo with subtasks", () => {
-    props.todo.subtask =  [
+    props.todo.subtask = [
       {
-        "id": 11,
-        "done": true,
-        "title": "Subtask one",
-        "todoId": 1
+        id: 11,
+        done: true,
+        title: "Subtask one",
+        todoId: 1,
       },
       {
-        "id": 12,
-        "done": false,
-        "title": "Subtask two",
-        "todoId": 1
-      }
+        id: 12,
+        done: false,
+        title: "Subtask two",
+        todoId: 1,
+      },
     ];
-    const {getAllByTestId, getByText, queryByTestId} = render(<MemoryRouter><Detail {...props} /></MemoryRouter>);
-    const items = getAllByTestId('listItem-container');
-    const todo = getByText('New Todo');
-    const st1 = getByText('Subtask one');
-    const st2 = getByText('Subtask two');
-    const noSubtasks = queryByTestId('detail-nosubtasks');
+    const { getAllByTestId, getByText, queryByTestId } = render(
+      <MemoryRouter>
+        <Detail {...props} />
+      </MemoryRouter>
+    );
+    const items = getAllByTestId("listItem-container");
+    const todo = getByText("New Todo");
+    const st1 = getByText("Subtask one");
+    const st2 = getByText("Subtask two");
+    const noSubtasks = queryByTestId("detail-nosubtasks");
     expect(items).toHaveLength(2);
     expect(todo).toBeInTheDocument();
     expect(st1).toBeInTheDocument();
     expect(st2).toBeInTheDocument();
     expect(noSubtasks).toBeNull();
   });
-  it("should be possible to change the state of the todo", () => {});
+  it("should be possible to change the state of the todo", async () => {
+    const { getByTestId } = render(<Detail {...props} />);
+
+    await act(async () => {
+      await fireEvent.click(getByTestId("done-button"));
+    });
+
+    expect(props.onSave).toHaveBeenCalledWith({
+      comment: "Todo Comment",
+      done: false,
+      id: 1,
+      title: "New Todo",
+    });
+  });
 });
