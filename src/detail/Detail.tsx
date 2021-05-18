62b010af3afd7ea71a0d6157f018508c137cf6ea
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-05-18 05:31:35 +0200

    Add Button to close Detail view

--- src/detail/Detail.tsx
+++ src/detail/Detail.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useHistory } from 'react-router-dom';
 import Done from '../done/Done';
 import Subtask from '../Subtask/Subtask.container';
 import { Todo, TodoInput } from '../Todo';
+import Button from '../util/button/Button';
 
 export type Props = {
   todo: Todo,
@@ -9,13 +11,14 @@ export type Props = {
 }
 
 const Detail: React.FC<Props> = ({todo, onSave}) => {
+  const history = useHistory();
   return (
     <div>
       { todo && <Done todo={todo} onSave={onSave} /> }
       <div data-testid="detail-title">{todo?.title}</div>
       <div data-testid="detail-comment">{todo?.comment}</div>
       {todo && todo.subtask && todo.subtask.length > 0 ? <Subtask todo={todo!} /> : <div data-testid="detail-nosubtasks">Keine Unteraufgaben</div>}
-      
+      <Button onClick={() => history.push('/')}>close</Button>
     </div>
   );
 };
