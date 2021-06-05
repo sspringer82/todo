2b514380d0a01e1ed3b48513048da84cc3a88639
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-06-05 08:05:55 +0200

    test: form with due

--- src/form/Form.test.tsx
+++ src/form/Form.test.tsx
@@ -39,8 +39,8 @@ describe("Form", () => {
           done: true,
           comment: "comment",
           category: { id: 1, title: "Private", color: "green" },
-          due: "",
-          created: "2021-06-01T08:15:00"
+          due: "2022-06-01T08:15:00",
+          created: "2021-06-01T08:15:00",
         },
       ])
     );
@@ -56,6 +56,7 @@ describe("Form", () => {
     expect(screen.getByTestId("title")).toHaveValue("first todo");
     expect(screen.getByTestId("done")).toBeChecked();
     expect(screen.getByTestId("comment")).toHaveValue("comment");
+    expect(screen.getByTestId("due")).toHaveValue("2022-06-01T08:15:00");
   });
   it("should save a new entry", async () => {
     await act(async () =>
@@ -73,6 +74,9 @@ describe("Form", () => {
       fireEvent.change(screen.getByTestId("comment"), {
         target: { value: "New Comment" },
       });
+      fireEvent.change(screen.getByTestId("due"), {
+        target: { value: "2022-06-01T08:15:00" },
+      });
     });
     expect(screen.getByTestId("title")).toHaveValue("New Title");
     expect(screen.getByTestId("done")).toBeChecked();
@@ -85,7 +89,7 @@ describe("Form", () => {
           done: true,
           comment: "New Comment",
           category: { id: 1, title: "Private", color: "green" },
-          due: "",
+          due: "2022-06-01T08:15:00",
           created: "2021-06-01T08:15:00",
         },
       ])
@@ -111,7 +115,7 @@ describe("Form", () => {
           done: true,
           comment: "Existing Comment",
           category: { id: 1, title: "Private", color: "green" },
-          due: "",
+          due: "2022-06-01T08:15:00",
           created: "2021-06-01T08:15:00",
         },
       ])
@@ -128,6 +132,7 @@ describe("Form", () => {
     expect(screen.getByTestId("title")).toHaveValue("Existing Todo");
     expect(screen.getByTestId("done")).toBeChecked();
     expect(screen.getByTestId("comment")).toHaveValue("Existing Comment");
+    expect(screen.getByTestId("due")).toHaveValue("2022-06-01T08:15:00");
     act(() => {
       fireEvent.change(screen.getByTestId("title"), {
         target: { value: "Existing Todo Modified" },
@@ -136,12 +141,16 @@ describe("Form", () => {
       fireEvent.change(screen.getByTestId("comment"), {
         target: { value: "Existing Comment Modified" },
       });
+      fireEvent.change(screen.getByTestId("due"), {
+        target: { value: "2022-08-01T08:15:00" },
+      });
     });
     expect(screen.getByTestId("title")).toHaveValue("Existing Todo Modified");
     expect(screen.getByTestId("done")).not.toBeChecked();
     expect(screen.getByTestId("comment")).toHaveValue(
       "Existing Comment Modified"
     );
+    expect(screen.getByTestId("due")).toHaveValue("2022-08-01T08:15:00");
     (fetch as any).mockResponseOnce(
       JSON.stringify([
         {
@@ -150,7 +159,7 @@ describe("Form", () => {
           done: false,
           comment: "Existing Comment Modified",
           category: { id: 1, title: "Private", color: "green" },
-          due: "",
+          due: "2022-08-01T08:15:00",
           created: "2021-06-01T08:15:00",
         },
       ])
@@ -166,6 +175,7 @@ describe("Form", () => {
     expect(body.title).toBe("Existing Todo Modified");
     expect(body.done).toBe(false);
     expect(body.comment).toBe("Existing Comment Modified");
+    expect(body.due).toBe("2022-08-01T08:15:00");
   });
   it("should show a list of subtasks", async () => {
     const response: Todo[] = [
@@ -176,7 +186,7 @@ describe("Form", () => {
         subtasks: [],
         category: { id: 1, title: "Private", color: "green" },
         due: "",
-        created: "2021-06-01T08:15:00"
+        created: "2021-06-01T08:15:00",
       },
       {
         id: 2,
